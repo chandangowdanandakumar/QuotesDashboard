98ffe4af063d413cd2b4785a6021a396aaea1776
Chandan Gowda <chandangowdanandakumar.outlook.com>
2021-10-13 23:28:44 -0700

    Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddquoteComponent } from './addquote/addquote.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/dashboard',pathMatch:"full"},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'addquote',component:AddquoteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  quotesList = [1,2,3,4,5,6,7,8,9,10]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
